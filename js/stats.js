9bd053e1f4d915e044c6bc8b29c2847d01148f3f
Justus Panitz <54219584+justus-21614@users.noreply.github.com>
2019-08-18 18:58:18 +0200

    Add stats.js txttype.js and update style.css and index.html

---
+++ js/stats.js
@@ -0,0 +1,14 @@
+var options = {
+  useEasing: true,
+  useGrouping: true,
+  separator: ',',
+  decimal: '.'
+};
+var demo1 = new CountUp("stat-tech", 0, 50, 0, 2.5);
+demo1.start()
+var demo2 = new CountUp("stat-jobs", 0, 60, 0, 3.5);
+demo2.start()
+var demo3 = new CountUp("stat-cleaned", 123, 2000, 0, 4);
+demo3.start()
+var demo4 = new CountUp("stat-exp", 0, 30, 0, 3.5);
+demo4.start()
